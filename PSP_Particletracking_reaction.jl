dae34c9be4bc9b3f1d3b522c14657212820a2246
asoppitt <asoppitt-42@hotmail.co.uk>
2021-10-21 15:18:19 +0100

    fixed a couple of bugs, added a toggle for PSP

--- PSP_Particletracking_reaction.jl
+++ PSP_Particletracking_reaction.jl
@@ -5,64 +5,72 @@ import Statistics as st
 
 Random.seed!(12345) #setting a seed
 
-n=19 #a convinent method of increascing resolution while maintaining 
+n=8 #a convinent method of increascing resolution while maintaining 
     # resolution ratios and number of particles per cell
 
-np = n*n*1000 # number of particles
-dt = 0.01  # time step
-nt = 100   # number of time steps
-length_domain = 1 #length of periodic element
-height_domain = 0.5
-phi_domain = [-1.2,1.2]
+np = n*n*4*5000 # number of particles
+dt = 0.005  # time step
+nt = 60   # number of time steps
+length_domain = 0.18 #length of periodic element
+height_domain = 0.75
+phi_domain = [0,1.2]
 omega_shape_init = 0.25
 omega_sigma_2 = 0.25
 psi_partions_num = 20 #number of partitions in 1 state space direction
 c_phi = 1.2
 c_t = 2
-u_max = 1
+u_max = 0
 function u_mean(y)
     u_max.*ones(np)#-4*u_max.*y.*(y.-height_domain)./height_domain^2
 end
-C_0 = 1.2  # rate parameter for velocity change
+C_0 = 10  # rate parameter for velocity change
 B=(1.5*C_0) #for reducing the langevin to a standard form - part of boundary conditions implementaion from Erban and Chapman 06
 x_res=n #number of cells in x dim
-y_res=ceil(Int, n/2)
-Initial_condition = "triple delta"
+y_res=4*n
+Initial_condition = "2 layers"
+PSP_off = true
+new_inflow = false
 
-bc_k=1
-omega_mean=10
-turb_e_init=1
+bc_k=0.75
+omega_mean=1
+turb_e_init=1.938
 T_omega = 1/(omega_mean); #approximation to frequency timescale
 phip = zeros((2, np, nt+1)) #scalar concentration at these points
+#ititialising position
 xp = length_domain*rand(Float64, (np,nt+1)) # position of particle in x-direction
 yp = height_domain*rand(Float64, (np,nt+1)) # position of paticle in y-direction
 
-#ititialising position
 if Initial_condition == "Uniform phi_1"
-    #ititialising at 1 for phi_1 for 1-D testing
-    phip[2,:,1] = 0.001*randn(np) #pdf can't find zeros
-    phip[1,:,1] .= 1;
+    function set_phi_as_ic(particles, nparticles, t_index)
+        phip[2,particles,t_index] = abs.(0.001*randn(nparticles)) #pdf can't find zeros
+        phip[1,particles,t_index] .= 1;
+    end
 elseif Initial_condition == "triple delta"
-    local delta_selector = rand(1:3, np)
-    local noise_term = randn(np)
+    function set_phi_as_ic(particles, nparticles, t_index)
+        local delta_selector = rand(1:3, nparticles)
+        local noise_term = randn(nparticles)
 
-    phip[1,delta_selector.=1,1] = -sqrt(3)/2 .+0.001 .*noise_term[delta_selector.=1]
-    phip[2,delta_selector.=1,1] .= -0.5
+        phip[1,particles[delta_selector.=1],t_index] = -sqrt(3)/2 .+0.0001 .*noise_term[delta_selector.=1]
+        phip[2,particles[delta_selector.=1],t_index] .= -0.5
 
-    phip[1,delta_selector.=2,1] = sqrt(3)/2 .+0.001 .*noise_term[delta_selector.=2]
-    phip[2,delta_selector.=2,1] .= -0.5
+        phip[1,particles[delta_selector.=2],t_index] = sqrt(3)/2 .+0.0001 .*noise_term[delta_selector.=2]
+        phip[2,particles[delta_selector.=2],t_index] .= -0.5
 
-    phip[1,delta_selector.=3,1] .= 0.001
-    phip[2,delta_selector.=3,1] = 1.0 .+0.001 .*noise_term[delta_selector.=3]
+        phip[1,particles[delta_selector.=3],t_index] .= 0.0001
+        phip[2,particles[delta_selector.=3],t_index] = 1.0 .+0.0001 .*noise_term[delta_selector.=3]
+    end
 elseif Initial_condition == "2 layers"
-    local noise_term = randn(np)
+    function set_phi_as_ic(particles, nparticles, t_index)
+        local noise_term = randn(nparticles)
 
-    phip[2,yp.>0.5,1] = 0.001*noise_term(yp.>0.5) 
-    phip[1,yp.>0.5,1] .= 1;
+        phip[2,particles[yp[particles,1].>0.5*height_domain],t_index] = abs.(0.0001*noise_term[yp[particles,1].>0.5*height_domain] )
+        phip[1,particles[yp[particles,1].>0.5*height_domain],t_index] .= 1;
 
-    phip[1,yp.<=0.5,1] = 0.001*noise_term(yp.<=0.5) 
-    phip[2,yp.<=0.5,1] .= 1;
+        phip[1,particles[yp[particles,1].<=0.5*height_domain],t_index] = abs.(0.0001*noise_term[yp[particles,1].<=0.5*height_domain] )
+        phip[2,particles[yp[particles,1].<=0.5*height_domain],t_index] .= 1;
+    end
 end
+set_phi_as_ic(1:np,np,1)
 
 omegap = zeros(np,nt+1) #turbulence frequency
 omega0_dist = Gamma(omega_mean/omega_shape_init,omega_shape_init)
@@ -156,15 +164,14 @@ end
 t_decorr_p = 1 ./(c_t.*omegap[phi_pm[1,:],1])
 t_decorr_m = 1 ./(c_t.*omegap[phi_pm[2,:],1])
 
-k=zeros(np)
-k= k.+turb_e_init
+k= ones.*turb_e_init
 
 #intitial vaules of velocity, maintainingconsitancy with energy
 uxp = randn(np).*sqrt.(2/3*k)
 uyp = randn(np).*sqrt.(2/3*k)
 
 function bc_absorbtion(abs_points::Vector{Int}, n_abs::Int, t_index::Int)
-    abs_k = bc_k.*phip[:, abs_points, t_index]
+    abs_k = bc_k.*ones(np)
     #K for Erban and Chapman approximation 
     P = zeros(n_abs,2)
     P[:,1] = abs_k[1,:].*sqrt.(B.*pi./(C_0.*k[abs_points]))
@@ -183,7 +190,7 @@ for t in 1:nt
         for j in 1:x_res
             in_x = x_edges[j].<xp[:,t].<x_edges[j+1]
             cell_particles = findall(in_x.&in_y)
-            k[cell_particles].=0.5*(st.mean(uxp[cell_particles].^2)+st.mean(uyp[cell_particles].^2))*1.5 #turb_e_init;
+            k[cell_particles].=turb_e_init#0.5*(st.mean(uxp[cell_particles].^2)+st.mean(uyp[cell_particles].^2))*1.5 #turb_e_init;
         end
     end
     global uxp = uxp+(-0.5*B*omega_mean*uxp)*dt+randn(np).*sqrt.(C_0.*k.*omega_mean.*dt); 
@@ -233,117 +240,125 @@ for t in 1:nt
     xpr_end = end_x .- length_domain #shifting particles back to begining
     xp[end_indicies,t+1] = xpr_end #replacing x coords
 
-    #resetting concentration of "new" particles
-    phip[2,end_indicies,t+1] = 0.001*randn(dim_end,1) #pdf can't find zeros
-    phip[1,end_indicies,t+1] .= 1
+    if new_inflow
+        #resetting concentration of "new" particles
+        set_phi_as_ic(end_indicies,dim_end,t+1)
 
-    #resetting energy of "new" particles
-    uxp[end_indicies] = randn(dim_end,1)*sqrt(2/3*turb_e_init)
-    uyp[end_indicies] = randn(dim_end,1)*sqrt(2/3*turb_e_init)
-    k[end_indicies] .= turb_e_init
+        #resetting energy of "new" particles
+        uxp[end_indicies] = randn(dim_end,1)*sqrt(2/3*turb_e_init)
+        uyp[end_indicies] = randn(dim_end,1)*sqrt(2/3*turb_e_init)
+        k[end_indicies] .= turb_e_init
+    end
 
     #bc at start (y=0) of domain
     start_indicies = xp[:,t+1].<=0 #index of particle with xp>length
     dim_start = sum(start_indicies)
 
-    xpr_start = -xp[start_indicies,t+1] #shifting particles back to begining
-    xp[start_indicies,t+1] = xpr_start #replacing x coords
-    uxp[start_indicies] = - uxp[start_indicies]
+    if new_inflow
+        xpr_start = -xp[start_indicies,t+1] #reflection
+        xp[start_indicies,t+1] = xpr_start #replacing x coords
+        uxp[start_indicies] = - uxp[start_indicies]
 
-    #resetting concentration of "new" particles
-    phip[2,start_indicies,t+1] = 0.001*randn(dim_start,1) #pdf can't find zeros
-    phip[1,start_indicies,t+1] .= 1
+        #resetting concentration of "new" particles
+        set_phi_as_ic(start_indicies,dim_start,t+1)
 
-    #resetting energy of "new" particles
-    uxp[start_indicies] = randn(dim_start,1)*sqrt(2/3*turb_e_init)
-    uyp[start_indicies] = randn(dim_start,1)*sqrt(2/3*turb_e_init)
-    k[start_indicies] .= turb_e_init
+        #resetting energy of "new" particles
+        uxp[start_indicies] = randn(dim_start,1)*sqrt(2/3*turb_e_init)
+        uyp[start_indicies] = randn(dim_start,1)*sqrt(2/3*turb_e_init)
+        k[start_indicies] .= turb_e_init
+    else
+        #true periodic bc
+        xpr_start = length_domain .+ xp[start_indicies,t+1] 
+        xp[start_indicies,t+1] = xpr_start #replacing x coords
+    end
 
 
     #PSP begins here
-    #E-M solver for omega
-    dw = sqrt(dt).*randn(np) #random draws
-    omegap[:,t+1] = omegap[:,t]-(omegap[:,t].-omega_mean)./T_omega.*dt + sqrt.(omegap[:,t].*(2*omega_sigma_2*omega_mean/T_omega)).*dw
-    omegap[:,t+1] = omegap[:,t+1].*(omegap[:,t+1].>0) #enforcing positivity
+    if PSP_off
+        dphi = zeros(2,np)
+    else
+        #E-M solver for omega 
+        dw = sqrt(dt).*randn(np) #random draws
+        omegap[:,t+1] = omegap[:,t]-(omegap[:,t].-omega_mean)./T_omega.*dt + sqrt.(omegap[:,t].*(2*omega_sigma_2*omega_mean/T_omega)).*dw
+        omegap[:,t+1] = omegap[:,t+1].*(omegap[:,t+1].>0) #enforcing positivity
 
-    #stepping the decorrelation times
-    global t_decorr_p = t_decorr_p.-dt;
-    global t_decorr_m = t_decorr_m.-dt;
-    #sets index of those to be renewed with 0 - which doesn't correspond to any particle
-    phi_pm[1,(t_decorr_p.<=0)] .= 0 #the t_decorrs are 2d for some reason
-    phi_pm[2,(t_decorr_m.<=0)] .= 0
+        #stepping the decorrelation times
+        global t_decorr_p = t_decorr_p.-dt;
+        global t_decorr_m = t_decorr_m.-dt;
+        #sets index of those to be renewed with 0 - which doesn't correspond to any particle
+        phi_pm[1,(t_decorr_p.<=0)] .= 0 #the t_decorrs are 2d for some reason
+        phi_pm[2,(t_decorr_m.<=0)] .= 0
 
+        #split into cells, compute centres/targets, run ODE step
+        for i in 1:y_res
+            in_y = y_edges[i].<=yp[:,t+1].<y_edges[i+1]
+            for j in 1:x_res
+                in_x = x_edges[j].<=xp[:,t+1].<x_edges[j+1]
+                cell_particles = findall(in_x.&in_y)
+                if 0 in cell_particles
+                    print("0 in cell_particles ", i,j,' ',t,'\n')
+                end
+                (length(cell_particles)==0) && throw(BoundsError(cell_particles))
+                #update pairs to ensure all are within the same bounds
+                p_nin = .!(in.(phi_pm[1,cell_particles],Ref(cell_particles)))
+                m_nin = .!(in.(phi_pm[2,cell_particles],Ref(cell_particles)))
+                pm_nin = p_nin .& m_nin
+                p_nin = xor.(p_nin , pm_nin)
+                m_nin = xor.(m_nin , pm_nin)
+                (sum(p_nin)>0)&&assign_pm_single(cell_particles[p_nin], cell_particles, 1, t)
+                (sum(m_nin)>0)&&assign_pm_single(cell_particles[m_nin], cell_particles, 2, t)
+                (sum(pm_nin)>0)&&assign_pm(cell_particles[pm_nin], cell_particles,t)
+                t_decorr_p[cell_particles[pm_nin.&p_nin]] = 1 ./(c_t.*omegap[phi_pm[1,cell_particles[pm_nin.&p_nin]],1])
+                t_decorr_m[cell_particles[pm_nin.&p_nin]] = 1 ./(c_t.*omegap[phi_pm[2,cell_particles[pm_nin.&m_nin]],2])
+            end 
+        end
+        phi_c = 0.5.*(phip[:,phi_pm[1,:],t]+phip[:,phi_pm[2,:],t])
+        diffusion = zeros(2,np)
+        diffusion[1,:] = c_phi.*0.5.*omegap[:,t].*(phip[1,:,t]-phi_c[1,:])
+        diffusion[1,:] = c_phi.*0.5.*omegap[:,t].*(phip[2,:,t]-phi_c[2,:])
+        reaction = zeros(2,np) # body reaction
+        dphi = (-diffusion .+ reaction).*dt
 
-    #split into cells, compute centres/targets, run ODE step
-    for i in 1:y_res
-        in_y = y_edges[i].<=yp[:,t+1].<y_edges[i+1]
-        for j in 1:x_res
-            in_x = x_edges[j].<=xp[:,t+1].<x_edges[j+1]
-            cell_particles = findall(in_x.&in_y)
-            if 0 in cell_particles
-                print("0 in cell_particles ", i,j,' ',t,'\n')
-            end
-            (length(cell_particles)==0) && throw(BoundsError(cell_particles))
-            #update pairs to ensure all are within the same bounds
-            p_nin = .!(in.(phi_pm[1,cell_particles],Ref(cell_particles)))
-            m_nin = .!(in.(phi_pm[2,cell_particles],Ref(cell_particles)))
-            pm_nin = p_nin .& m_nin
-            p_nin = xor.(p_nin , pm_nin)
-            m_nin = xor.(m_nin , pm_nin)
-            (sum(p_nin)>0)&&assign_pm_single(cell_particles[p_nin], cell_particles, 1, t)
-            (sum(m_nin)>0)&&assign_pm_single(cell_particles[m_nin], cell_particles, 2, t)
-            (sum(pm_nin)>0)&&assign_pm(cell_particles[pm_nin], cell_particles,t)
-            t_decorr_p[cell_particles[pm_nin.&p_nin]] = 1 ./(c_t.*omegap[phi_pm[1,cell_particles[pm_nin.&p_nin]],1])
-            t_decorr_m[cell_particles[pm_nin.&p_nin]] = 1 ./(c_t.*omegap[phi_pm[2,cell_particles[pm_nin.&m_nin]],2])
-        end 
-    end
-    phi_c = 0.5.*(phip[:,phi_pm[1,:],t]+phip[:,phi_pm[2,:],t])
-    diffusion = zeros(2,np)
-    diffusion[1,:] = c_phi.*0.5.*omegap[:,t].*(phip[1,:,t]-phi_c[1,:])
-    diffusion[1,:] = c_phi.*0.5.*omegap[:,t].*(phip[2,:,t]-phi_c[2,:])
-    reaction = zeros(2,np) # body reaction
-    dphi = (-diffusion .+ reaction).*dt
-
-    # ensuring mean 0 change
-    # generating a random orthonormal basis
-    # is 2-d so genrate a random unit vector from an angle and proceed based
-    # on that
-    angle = 2*pi*rand(1)[1];
-    e_1 = [cos(angle),sin(angle)]
-    handedness = sb.sample([-1,1],1)[1] #randomly choose betwen left or right handed system
-    e_2 = handedness*[e_1[2],-e_1[1]]
-    T=zeros(2,2)
-    T[:,1] = e_1  #coord transform matrix
-    T[:,2] = e_2
-    dphi = T\dphi  #transform to new coords
-    #performing adjustment to mean 0
-    corr_factor = zeros(2,np)
-    for i in 1:y_res
-        in_y = y_edges[i].<=yp[:,t+1].<y_edges[i+1]
-        for j in 1:x_res
-            in_x = x_edges[j].<=xp[:,t+1].<x_edges[j+1]
-            cell_particles = findall(in_x.&in_y)
-            for phi_i=1:2
-                phi_mean = mean(dphi[phi_i,cell_particles])
-                if phi_mean != 0
-                    cell_points_pos = dphi[phi_i,cell_particles].>0
-                    cell_points_neg = dphi[phi_i,cell_particles].<0
-                    phi_pos_mean = mean(cell_points_pos.*dphi[phi_i,cell_particles])
-                    phi_neg_mean = mean((cell_points_neg).*dphi[phi_i,cell_particles])
-                    if phi_mean>0
-                        corr_factor[phi_i,cell_particles]=.-cell_points_pos*(phi_neg_mean./phi_pos_mean) + (1 .- cell_points_pos)
-                        (any(isinf.(corr_factor[phi_i,cell_particles])) || any(isnan.(corr_factor[phi_i,cell_particles]))) && print(" +corr ")
-                    else
-                        corr_factor[phi_i,cell_particles]=.-(cell_points_neg)*(phi_pos_mean./phi_neg_mean) + (1 .- cell_points_neg)
-                        (any(isinf.(corr_factor[phi_i,cell_particles])) || any(isnan.(corr_factor[phi_i,cell_particles]))) && print(" -corr ")
+        # ensuring mean 0 change
+        # generating a random orthonormal basis
+        # is 2-d so genrate a random unit vector from an angle and proceed based
+        # on that
+        angle = 2*pi*rand(1)[1];
+        e_1 = [cos(angle),sin(angle)]
+        handedness = sb.sample([-1,1],1)[1] #randomly choose betwen left or right handed system
+        e_2 = handedness*[e_1[2],-e_1[1]]
+        T=zeros(2,2)
+        T[:,1] = e_1  #coord transform matrix
+        T[:,2] = e_2
+        dphi = T\dphi  #transform to new coords
+        #performing adjustment to mean 0
+        corr_factor = zeros(2,np)
+        for i in 1:y_res
+            in_y = y_edges[i].<=yp[:,t+1].<y_edges[i+1]
+            for j in 1:x_res
+                in_x = x_edges[j].<=xp[:,t+1].<x_edges[j+1]
+                cell_particles = findall(in_x.&in_y)
+                for phi_i=1:2
+                    phi_mean = mean(dphi[phi_i,cell_particles])
+                    if phi_mean != 0
+                        cell_points_pos = dphi[phi_i,cell_particles].>0
+                        cell_points_neg = dphi[phi_i,cell_particles].<0
+                        phi_pos_mean = mean(cell_points_pos.*dphi[phi_i,cell_particles])
+                        phi_neg_mean = mean((cell_points_neg).*dphi[phi_i,cell_particles])
+                        if phi_mean>0
+                            corr_factor[phi_i,cell_particles]=.-cell_points_pos*(phi_neg_mean./phi_pos_mean) + (1 .- cell_points_pos)
+                            (any(isinf.(corr_factor[phi_i,cell_particles])) || any(isnan.(corr_factor[phi_i,cell_particles]))) && print(" +corr ")
+                        else
+                            corr_factor[phi_i,cell_particles]=.-(cell_points_neg)*(phi_pos_mean./phi_neg_mean) + (1 .- cell_points_neg)
+                            (any(isinf.(corr_factor[phi_i,cell_particles])) || any(isnan.(corr_factor[phi_i,cell_particles]))) && print(" -corr ")
+                        end
                     end
                 end
             end
         end
+        dphi = corr_factor.*dphi
+        dphi = T*dphi #return to old coords
     end
-    dphi = corr_factor.*dphi
-    dphi = T*dphi #return to old coords
-
 
     phip[:,:,t+1] = phip[:,:,t]+dphi
     if !(Initial_condition == "triple delta")
@@ -354,5 +369,5 @@ for t in 1:nt
 
 end
 
-write("new_reaction_tri_del", f_phi)
+write("PSP_off_2_layers_aspersummer_c_0_10", f_phi)
 print("Success")
